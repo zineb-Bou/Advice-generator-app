54d1611b206e5493a75f29f74499055d1084a933
Zineb <boutaazineb08@gmail.com>
2022-03-06 21:50:14 +0100

    Fixing a11y

--- src/components/card.js
+++ src/components/card.js
@@ -69,7 +69,7 @@ export default function AdviceCard() {
       >
         ❝ {advice.advice}❞
       </p>
-      <svg width={295} height={16} xmlns="http://www.w3.org/2000/svg">
+      <svg aria-hidden="true" width={295} height={16} xmlns="http://www.w3.org/2000/svg">
         <g fill="none" fillRule="evenodd">
           <path fill="#4F5D74" d="M0 8h122v1H0zm173 0h122v1H173z" />
           <g transform="translate(138)" fill="#CEE3E9">
